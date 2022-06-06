9e7e27922b56d8ea2445ecce72b619b5c47ea27a
Ewedo <ewedoasfaw@gmail.com>
2022-06-06 00:08:23 -0700

    UNIX is simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stadio.h>
+#include <unistd.h>
+
+/**
+  *main - Entry point
+  *return: always 1
+  *
+  */
+
+int main(void)
+{
+	write(2, "and that piece of atr is useful" - Dora Korpar, 2015-10-19\n,);
+	return (1);
+}
