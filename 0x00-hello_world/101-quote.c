13b1c8560f91ee7f7e47ea3f54f65b3237604822
Ewedo <ewedoasfaw@gmail.com>
2022-06-06 00:53:21 -0700

    UNIX is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 
 /**
   *main - Entry point
-  *Rturn main: Always 1
+  *Return: Always 1
   *
   */
 
 int main(void)
 {
-	Write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
