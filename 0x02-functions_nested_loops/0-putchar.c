a24a6b81cee3c4fb18f1623e00be09163b39271d
Ewedo <ewedoasfaw@gmail.com>
2022-06-09 02:46:37 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 
-int main() {
-	char chars[] = {'_','p','u','t','c','h','a','r'};
+int main(void);
+{
+	char chars[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 
 	int i = 0;
-	while (i<8){
+
+	while (i<8)
+	{
 		_putchar(chars[i]);
 		i++;
 	}
 	
 	_putchar('\n');
 
-	return 0;
+	return (0);
 }
 
-
-/*
-int _putchar(char c){
-	return (write(1, &c, 1));
-}
-*/
