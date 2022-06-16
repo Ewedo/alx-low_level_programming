89f2abd82cd9a97e2c6958c3fe64404b0cbf4c09
Ewedo <ewedoasfaw@gmail.com>
2022-06-16 16:28:58 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/**
+ * add - addition operation
+ * @i: The number to be added.
+ * @j: The number to be added.
+ * Return: the sum of two numbers.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
