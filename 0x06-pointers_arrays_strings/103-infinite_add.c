1c52be1dfbb006da7f6ffc6d9cdd2dc7e79fb7d7
Ewedo <ewedoasfaw@gmail.com>
2022-06-16 17:16:58 -0700

    10. A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,31 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+void _strrev(char *str);
+
 /**
- * infinite_add - function with 4 arguments
- * @n1: char type pointer argument
- * @n2: char type pointer argument
- * @r: char type pointer argument
- * @size_r: int type argument
+ * infinite_add - a function that adds two numbers
+ * @n1: a pointer to first input string
+ * @n2: a pointer to second input string
+ * @r: pointer to buffer where result is stored
+ * @size_r: size for the buffer
  *
- * Description: adds two numbers from string
- * Return: sum of two integers
+ * Return: return pointer to the result
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int count, count2;
+	char *str1, *str2;
+	int i, m, carry, len;
 
-	while (n1[count] != '\0')
-		count++;
-	while (n2[count2] != '\0')
-		count2++;
-
-	*r = *(r + size_r);
-	while (n1[count] > 0 || n1[count2] > 0)
+	str1 = (char *)malloc(strlen(n1) + 1);
+	str2 = (char *)malloc(strlen(n2) + 1);
+	assert(str1 != NULL);
+	assert(str2 != NULL);
+	i = m = carry = 0;
+	if (strlen(n1) > strlen(n2))
 	{
-		if (n1[count] + n2[count2] > 0)
-			*r = n1[count - 1] + n2[count2 - 1] + 1;
-		else
-			*r = n1[count] + n2[count2];
-		count--;
-		count2--;
+		strcpy(str1, n1);
+		strcpy(str2, n2);
 	}
-	return (r);
-}
+	else
+	{
+		strcpy(str1, n2);
+		strcpy(str2, n1);
+	}
+	len = strlen(str1);
+	if (len + 1 > size_r)
+	{
+		r = '\0';
+		return (r);
+	}
+	else
+	{
+		_strrev(str1);
+		_strrev(str2);
+		m = str1[0] - '0' + str2[0] - '0';
+		if (m >= 10)
+			carry = 1;
+		r[0] = m % 10 + '0';
+		for (i = 1; str2[i] != '\0'; i++)
+		{
+			m = str1[i] - '0' + str2[i] - '0' + carry;
+			if (m >= 10)
+				carry = 1;
+			else
+				carry = 0;
+			r[i] = m % 10 + '0';
+		}
+		for ( ; str1[i] != '\0'; i++)
+		{
+			m = str1[i] - '0' + carry;
+			if (m >= 10)
+				carry = 1;
+			else
+				carry = 0;
+			r[i] = m % 10 + '0';
+		}
+		if (carry == 1 && (len + 1 >= size_r))
+		{
+			r = '\0';
+		}
+	}		
