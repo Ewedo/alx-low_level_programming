ab626779869e8fce8441a48712f9e2f442b7e0ab
Ewedo <ewedoasfaw@gmail.com>
2022-06-16 18:14:26 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+ #include <unistd.h>
+#include "main.h"
+
+/**
+ * print_numbers -  a function that prints the numbers, from 0 to 9.
+ * no variable input
+ *
+ * Ruturn: void
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
