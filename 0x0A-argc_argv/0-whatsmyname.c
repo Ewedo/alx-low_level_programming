9dac013c21514658354a45ffeecec9bff829b7aa
Ewedo <ewedoasfaw@gmail.com>
2022-06-26 06:14:04 -0700

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,10 +9,8 @@
  * Return: 0 on success
  */
 
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	(void)argc;
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
