1d372482d4ca5f698d1e475a16e90915796f0ac9
Ewedo <ewedoasfaw@gmail.com>
2022-07-05 07:59:16 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -15,7 +15,7 @@ int main(void)
 
         while (i < 10)
         {
-                putchar(i);/** this is undeclerd char*/
+                putchar(i);
         }
 
         printf("Infinite loop avoided! \\o/\n");
