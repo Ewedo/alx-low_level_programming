4a7fc07b02cdc8d8147b94a019b1024a8069b0ab
Ewedo <ewedoasfaw@gmail.com>
2022-07-06 12:13:06 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,3 +1,11 @@
-int _putchar(char c)
-void simple_print_buffer(char *buffer, unsigned int size)
+
+#include <unistd.h>
+#include <stdio.h>
+int _putchar(char c);
+void simple_print_buffer(char *buffer, unsigned int size);
 char *create_array(unsigned int size, char c);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+
